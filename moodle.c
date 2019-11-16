8fb1c3eb29f0f7e3942df7e451aa58d41ec49cfa
wallaceice <50365294+wallaceice@users.noreply.github.com>
2019-11-16 18:35:32 -0300

    Update moodle.c

--- moodle.c
+++ moodle.c
@@ -2,6 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+
+int opcao;
+int Login();
+int ConsultaD();
+void Notas(int ra);
+void Matricula(int RA);
+int Semestre(int ra);
+int Creditos(int ra,int sem_atual);
+int credito_disciplina(char disciplina[8]);
+int Requisitos(int ra,char disciplina[9],int sem_atual);
+
+
+void limparbuffer(void){
+	char c;
+	while ((c = getchar()) != '\n' && c!= EOF);
+}
+
 typedef struct Disciplina{
     char codigo[8];
     char *nome;
@@ -19,104 +37,356 @@ typedef struct Aluno{
 
 typedef struct matricula{
     int ra;
-    int CodigDisciplina;
+    char CodigDisciplina[8];
     int semestre;
-    int nota;
-    int faltas;
+    float nota;
+    float faltas;
 
 }matricula;
 
-typedef struct ord_matricula{
 
-    matricula *vet;
+int cont_alunos(){
 
-}ordenacao;
+	int cont = 0;
 
-int opcao;
-int Login();
-int ConsultaD();
+	int Ra,semestre,sem_txt;
+	char disciplina[8];
+	float nota, falta;
 
+	FILE *fp = fopen("AlunosDisciplinas.txt","r");
+	if (fp == NULL){
+		perror("Erro: função cont alunos FILE");
+	}
+	while(fscanf(fp,"%d;%[^;];%d;%f;%f",&Ra,disciplina,&sem_txt,&nota,&falta)!=EOF){
+		cont++;
+	}
 
-
-int compara(const void *a,const void *b){
-    matricula *aux = *(matricula **) a;
-    matricula *aux2 = *(matricula **) b;
-    
-    if(aux->valor > aux2->valor)
-        return 1;
-
-    else{
-        if(aux->valor < aux2->valor)
-            return -1;
-    }
-
-    return 0;    
-
+	return cont;
 }
 
-void OrdenaMatricula(){
 
-    char *sub;
-    char *linha[256];
-    char linha_fake[256];
-    int c = 0;
+void Notas(int ra){
 
-    ordenacao aluno;
-    aluno->vet = (matricula*) malloc (10000 * sizeof(matricula));
 
-    FILE *fp = fopen("AlunosDisciplinas.txt", "r");
+	int Ra,semestre,sem_txt;
+	char disciplina[8];
+	float nota, falta;
+	int count_alunos, credito_txt;
+	char nome[60],codigo[8];
 
-    while(fgets(linha,256,fp)){
-        if(strcmp(linha_fake,linha) == 0){
-            break;
-        }
-        strcpy(linha_fake,linha);
+	FILE *fp[2];
 
-        sub = strtok(linha,";");
-        aluno->vet[c]->ra = atoi(sub);
-        sub = strtok(NULL,";");
-        strcpy(aluno->vet[c]->CodigDisciplina,sub);
-        sub = strtok(NULL,";");
-        aluno->vet[c]->semestre = atoi(sub);
-        sub = strtok(NULL,";");
-        aluno->vet[c]->nota = atoi(sub);
-        sub = strtok(NULL,";");
-        aluno->vet[c]->faltas = atoi(sub);
-        c++;
-    }
-    fclose(fp);
+	do{
+		printf("Menu de Alteração de notas e faltas: \n");
+		printf("Digite o semestre:\n");
+		scanf("%d",&semestre);
+		limparbuffer();
+		sem_txt = Semestre(ra);
+		if(semestre > sem_txt){
+			printf("voce nao esta cursando nenhuma disciplina neste semestre\n\n\nTente novamente\n\n\n\n");
+		}
+	}while(semestre > sem_txt);
+	
 
-    qsort(aluno->vet,c,sizeof(ordenacao *),compara);
-    FILE *fp = fopen("AlunosDisciplinas.txt", "w");
-    for(int j  = 0; j<c; j++){
-        fprintf(fp"%d,"aluno->vet[c]->ra);
-        fprintf(fp"%s,"aluno->vet[c]->CodigDisciplina);
-        fprintf(fp"%d,"aluno->vet[c]->semestre);
-        fprintf(fp"%d,"aluno->vet[c]->nota);
-        fprintf(fp"%d,"aluno->vet[c]->faltas);
+	fp[0] = fopen("AlunosDisciplinas.txt","r");
 
-    }
-    fclose(fp);
-    
-    
+	if (fp[0] == NULL){
+ 		perror("Erro: Function notas FILE ALUNOS DISIPLINA");
+	}
+	printf("Disciplinas:\n");
+
+	while(fscanf(fp[0],"%d;%[^;];%d;%f;%f",&Ra,disciplina,&sem_txt,&nota,&falta)!=EOF){
+		if(Ra == ra && sem_txt == semestre){
+			fp[1] = fopen("Disciplinas.txt","r");
+			if (fp[1] == NULL){
+ 				perror("Erro: funcao Notas FILE Dentro do WHILE");
+			}
+			while(fscanf(fp[1],"%[^;];%[^;];%d\n",codigo,nome,&credito_txt)!= EOF){
+				if(strcmp(disciplina, codigo)== 0)
+					printf("%s-%s-Nota: %.1f-,Falta(%%):%.1f\n",codigo,nome,nota,falta);
+			}fclose(fp[1]);
+		}
+	}
+
+	fclose(fp[0]);
+
+	int i = cont_alunos();
+	matricula alunos[i];
+	
+
+	i = 0;
+	fp[0] = fopen("AlunosDisciplinas.txt","r");
+	if (fp[0] == NULL){
+		perror("Erro: NOTAS SEgunda abertura ALUNOS_D");
+	}
+	while(fscanf(fp[0],"%d;%[^;];%d;%f;%f\n",&Ra,disciplina,&sem_txt,&nota,&falta)!=EOF){
+		alunos[i].ra = Ra;
+		strcpy(alunos[i].CodigDisciplina,disciplina);
+    	alunos[i].semestre = sem_txt;
+    	alunos[i].nota = nota;
+    	alunos[i].faltas = falta;
+
+		i++;
+	}
+	fclose(fp[0]);
+
+	printf("Digite o codigo da disciplina que deseja fazer alteração:\n");
+	scanf("%s",codigo);
+	limparbuffer();
+	for(int c = 0;c < i;c++){
+		if(alunos[c].ra == ra && alunos[c].semestre == semestre && strcmp(codigo,alunos[c].CodigDisciplina)== 0){
+			printf("Nota atual: %.1f\n",alunos[c].nota);
+			printf("Faltas(%%) atualmente: %.1f\n",alunos[c].faltas);
+			printf("Inserir nova nota:\n");
+			scanf("%f",&alunos[c].nota);
+			limparbuffer();
+			printf("Inserir nova falta:\n");
+			scanf("%f",&alunos[c].faltas);
+			limparbuffer();			
+			printf("Atualização realizada com sucesso\n");			
+		}
+	}
+	fp[0] = fopen("AlunosDisciplinas.txt","w");
+	if (fp[0] == NULL){
+		perror("Erro: NOTAS TERceira ABERTURA ALUNOS _ D");
+	}
+		for(int c = 0;c < i;c++){
+		fprintf(fp[0],"%d;%s;%d;%.1f;%.1f\n",alunos[c].ra,alunos[c].CodigDisciplina,
+										alunos[c].semestre,alunos[c].nota,alunos[c].faltas);
+		}
+	fclose(fp[0]);
+
 }
-void Matricula(int ra){
-    OrdenaMatricula();
-    int semestre;
-    int semestre_arq;
-    int disciplina;
-    FILE *fp = fopen("AlunosDisciplinas.txt", "a+");
-    
-    printf("\nMenu de Realizar Matrícula\n");
-    printf("Digite o semestre: \n");
-    scanf("%d", &semestre);
-    do{
-        printf("Digite a disciplina: \n");
-        scanf("%s", &disciplina);
-        
-    }while(disciplina != "XX000");
-    printf("Transação efetuada com sucesso");
+int Semestre(int ra){
+	//printf("Erro SEMESTRe ra %d!\n",ra);
+	int Ra,semestre = 0,sem;
+	char disciplina[8];
+	float nota, falta;
 
+	FILE *fp = fopen("AlunosDisciplinas.txt", "r");
+	if (fp == NULL){
+ 		perror("Erro: SEMESTRE FILE ALUNO_D"); //a mensagem de erro vai ser impressa no terminal
+	}
+
+
+	while(fscanf(fp,"%d;%[^;];%d;%f;%f",&Ra,disciplina,&sem,&nota,&falta)!=EOF){
+		//printf("%d;%s;%d;%.1f;%.1f\n\n",Ra,disciplina,sem,nota,falta);
+		if( ra == Ra && sem>semestre)
+			semestre = sem;
+	}
+	fclose(fp);
+
+	return semestre;
+}
+
+int Creditos(int ra,int sem_atual){
+
+	//printf("Erro Creditos ra %d!  sem atual %d!\n",ra,sem_atual);
+
+	int creditos = 0;
+	int ra_txt;
+	char materia[8];
+	float nota,falta;
+	int semestre;
+
+	char codigo[8];
+	char nome[60];
+	int credito_txt;
+	FILE * fp[2];
+	
+	fp[0] = fopen("AlunosDisciplinas.txt","r");
+		if (fp[0] == NULL){
+	 		perror("Erro: CREDITOS ALUNO_D "); //a mensagem de erro vai ser impressa no terminal
+		}
+
+	while(fscanf(fp[0],"%d;%[^;];%d;%f;%f\n",&ra_txt,materia,&semestre,&nota,&falta)!= EOF){
+		//printf("AlunosDisciplinas:%d;%s;%d;%.1f;%.1f\n\n",ra_txt,materia,semestre,nota,falta);
+		if(ra_txt == ra && semestre == sem_atual){
+			fp[1] = fopen("Disciplinas.txt","r");
+			if (fp[1] == NULL){
+		 		perror("Erro: CREDITOS DISCIPLINA");
+			}
+			while(fscanf(fp[1],"%[^;];%[^;];%d\n",codigo,nome,&credito_txt)!= EOF){
+				//printf("Disciplinas %s;%s;%d\n\n",codigo,nome,credito_txt);
+				if(strcmp(codigo,materia) == 0){
+					creditos += credito_txt;
+					break;
+				}
+			}fclose(fp[1]);
+		}
+	}
+	fclose(fp[0]);
+	return creditos;
+}
+int credito_disciplina(char disciplina[9]){
+	//printf("Erro Creditos Dissciplina  %s!\n",disciplina);
+	
+	char codigo[8];
+	char nome[60];
+	int credito;
+
+	FILE  * fp = fopen("Disciplinas.txt","r");
+
+	while(fscanf(fp,"%[^;];%[^;];%d\n",codigo,nome,&credito)!= EOF){
+	//printf("Erro Creditos Dissciplina  %s!\n",codigo);
+		if(strcmp(codigo,disciplina) == 0){
+			return credito;
+		}
+	}
+	fclose(fp);
+	return 0;
+}
+
+int Requisitos(int ra,char disciplina[9],int sem_atual){
+	//printf("Erro Requisitos ra %d!  sem atual %s!\n",ra,disciplina);
+
+	char semnada[] = "sem_nada";
+	char codigo[8], prerequisitos[2][9], txt;
+	int ra_txt,semestre;
+	char materia[8];
+	float nota = 0, falta = 100;
+	int conclusao = 0;// 0 nao atende aos requisitos, 1 atende aos requisitos, 2 ja foi aprovado na disciplina cadastrando
+
+
+	strcpy(prerequisitos[1],"sem_nada");
+	//printf("%s\n",prerequisitos[1]);
+
+	FILE * fp;
+	fp = fopen("Prerequisitos.txt","r");
+
+	while(fscanf(fp,"%[^;];%s",codigo,prerequisitos[0])!= EOF){
+		if(strcmp(codigo,disciplina) == 0){
+			//printf("Prerequisitos{ 1!} !!%s!!\n\n",prerequisitos[0]);
+			fscanf(fp,"%c",&txt);
+			if(txt == ' '){
+				fscanf(fp,"%s\n",prerequisitos[1]);
+				//printf("Prerequisitos{ 1!} !!%s!!\n\n",prerequisitos[1]);
+			}break;
+		}fscanf(fp,"\n");
+	}
+	fclose(fp);
+	if(strcmp(codigo,disciplina) != 0){
+		conclusao = 3;
+		return conclusao;
+	}
+
+
+	fp = fopen("AlunosDisciplinas.txt","r");
+
+	while(fscanf(fp,"%d;%[^;];%d;%f;%f\n",&ra_txt,materia,&semestre,&nota,&falta)!= EOF){
+		//printf("ALUnodisciplia.xtx: !!%d;%s;%d;%.1f;%.1f!!!\n",ra_txt,materia,semestre,nota,falta);
+		if(ra == ra_txt){
+			if(strcmp(materia,disciplina) == 0){
+				if(nota >=5.0 && falta <25.0 || semestre == sem_atual){
+					conclusao = 2;
+					return conclusao;
+				}
+			}
+			if(strcmp(materia,prerequisitos[0]) == 0){
+				if(nota >=5.0 && falta <25.0){
+					conclusao = 1;
+				}else{
+					conclusao = 0;
+				}
+			}
+			if(strcmp(materia,prerequisitos[1]) == 0){
+				if(nota >=5.0 && falta <25.0){
+					conclusao = 1;
+				}else{
+					conclusao = 0;
+				}return conclusao;	
+			}				
+		}		
+	}
+	if(strcmp(prerequisitos[0],"Nãohá") == 0){
+		conclusao = 1;
+	}
+
+	fclose(fp);
+
+	return conclusao;
+}
+void Matricula(int RA){
+
+
+	int returne = 0;
+    int semestre,sem_atual, c = 0;
+    char disciplina[128][8];
+	int requisito;// verifica se o aluno passou nos prerequisitos
+	int creditos = 0; 
+	float nota = 0, falta = 0;
+	int ra = RA;
+
+	do{
+	    printf("\n####Menu de Realizar Matrícula#####\n\n");
+		printf("Para sair digite XX000\n");
+    	printf("Digite o semestre: \n");
+		scanf("%d",&semestre);
+		limparbuffer();
+		sem_atual = Semestre(ra);
+		creditos = Creditos(ra,semestre);// contagem creditos no semestre atual
+		//printf("QNT CREDITOS: ###%d###\n",creditos);
+
+		if(sem_atual > semestre || semestre == 0){
+			printf("Semestre requerido inferior ao semestre mais atual\n");
+			printf("Tente novamente:\n");
+		}
+
+	}while(sem_atual > semestre || semestre == 0);
+
+	//printf("####Depoois do SEMESTRE do erro###\n\n");
+	while(strcmp(disciplina[c],"XX000") != 0 && c <128){
+		if (returne == 1){
+			break;
+		}
+
+		printf("Digite a disciplina:\n");
+		scanf("%s",disciplina[c]);
+		limparbuffer();
+		if(strcmp(disciplina[c],"XX000") == 0)
+			break;
+		requisito = Requisitos(ra,disciplina[c],semestre);
+		switch(requisito){
+			case 0:
+				printf("Voce não possui os requisitos necessarios para cursar a disciplina %s\n",disciplina[c]);
+				strcpy(disciplina[c],"SKIP");
+				break;
+
+			case 1:
+				creditos += credito_disciplina(disciplina[c]);// credito por disciplina
+				if(creditos > 32){
+					printf("Limite de creditos excedido");
+					returne = 1;
+					break;
+				}
+				break;
+			case 2:
+				printf("Voce já esta matriculado ou já foi aprovado na disciplina %s\n",disciplina[c]);
+				strcpy(disciplina[c],"SKIP");
+				break;
+
+			case 3:
+				printf("Disciplina %s nao encontrada ou inexistente\n",disciplina[c]);
+				strcpy(disciplina[c],"SKIP");
+				break;
+			default:
+				break;		
+		}				
+		c++;
+	}
+	
+	FILE  *fp = fopen("AlunosDisciplinas.txt","a+");
+	for(int i = 0;i< c;i++){
+			//printf("Disciplina: %d : ## %s ##\n",i,disciplina[i]);
+		if(strcmp(disciplina[i],"SKIP") != 0){
+			//printf("%d;%s;%d;%.1f;%.1f\n",ra,disciplina[i],semestre,nota,falta);
+			fprintf(fp,"%d;%s;%d;%.1f;%.1f\n",ra,disciplina[i],semestre,nota,falta);
+		}
+	}
+	fclose(fp);
+
+	if (returne != 1)
+	    printf("Transação efetuada com sucesso\n");
 }
 
 void Cadastro(){
@@ -132,7 +402,7 @@ void Cadastro(){
     printf("Digite o RA do Aluno:\n");
     scanf("%d",&test.ra);
     printf("Digite o nome do Aluno:\n");
-    getchar();
+    limparbuffer();
     fgets(test.nome,256,stdin);
     printf("Digite o login do aluno:\n");
     fgets(test.login,32,stdin);
@@ -150,66 +420,45 @@ void Cadastro(){
     fclose(fp);
 }
 
-/*int menuP(){
-    printf("\nSistema de Gerência de Disciplinas Semestrais\n\n");
-    printf("1- Cadastro de Aluno\n");
-    printf("2- Login\n");
-    printf("0- Sair do menu\n");
-    printf("Digite a opção:\n");
-    scanf("%d",&opcao);
-    getchar();
 
-    switch(opcao){
-        case 0:
-            return 0;
-
-        case 1:
-            Cadastro();        
-            break;
-        
-        case 2:
-            Login();
-            break;
-
-        default:
-            printf("Opcao inválida");
-            break;
-    }
-}*/
-
-int menu(){
+int menu(int ra){
     int a;
-    printf("\nMenu de Opcoes\n\n");
-    printf("1- Cadastrar de Alunos\n");
-    printf("2- Consultar Disciplinas\n");
-    printf("3- Realizar Matrícula\n")
-    printf("4- Atualizar Nota e Falta\n")
-    printf("0- Sair\n");
+	
 
-    scanf("%d",&a);
-    getchar();
-
-    switch(a){
-        case 1:
-            Cadastro();// cadastro aluno em disciplinsa nao implementado
-            break;
+	do{
+	    printf("\nMenu de Opcoes\n\n");
+	    printf("1- Cadastrar de Alunos\n");
+	    printf("2- Consultar Disciplinas\n");
+	    printf("3- Realizar Matrícula\n");
+	    printf("4- Atualizar Nota e Falta\n");
+	    printf("0- Sair\n");
+	
+	    scanf("%d",&a);
+	    limparbuffer();
+	
+	    switch(a){
+    	    case 1:
+    	        Cadastro();
+    	        break;
         
-        case 2:
-            ConsultaD();        
-            break;
-        case 3:
-            Matricula();
-            break;
-        case 4:
-            Notas();
-            break;
-        case 0:
-            return 0;
+    	    case 2:
+    	        ConsultaD();        
+    	        break;
+    	    case 3:
+    	        Matricula(ra);
+    	        break;
+    	    case 4:
+    	        Notas(ra);
+    	        break;
+    	    case 0:
+    	        return 0;
+	
+    	    default:
+    	        printf("Opcao inválida");
+    	        break;
+    	}
+	}while(a != 0);
 
-        default:
-            printf("Opcao inválida");
-            break;
-    }
 }
 
 int Login(){
@@ -243,14 +492,10 @@ int Login(){
         sub = strtok(NULL,",");
         strcpy(senha_correta,sub);
 
-/*        int compara_log = strcmp(login,log_correto);
-        printf("Compara log: %d \nlogin: \nlog_correto:\n%s!\n%s!\n",compara_log,login,log_correto);
-        compara_log = strcmp(senha,senha_correta);
-        printf("Compara_senha: %d\nsenha:\n senhacorreta:\n%s!\n%s!\n",compara_log,senha,senha_correta);
-*/
+
         if(strcmp(login,log_correto) == 0 && strcmp(senha,senha_correta) == 0){// compara login e senha de uma mesma linha dentro do arquivo com a entrada do usuario
             printf("\nBem vindo %s\n\n", nome);
-            menu();
+            menu(ra);
             break;
         }
     }
@@ -315,6 +560,7 @@ int ConsultaD(){
         sub = strtok(linha,";");
         strcpy(cod_correto,sub);
         sub = strtok(NULL,";");
+
         //printf("%s\n", sub);
         //strcpy(Prerequisito,sub);
         
@@ -411,7 +657,7 @@ int ConsultaD(){
     fclose(fp);
     codS = 0;
     
-    menu();
+    menu(codS);
 }
 
 int main(){
@@ -429,7 +675,7 @@ int main(){
     printf("0- Sair do menu\n");
     printf("Digite a opção:\n");
     scanf("%d",&opcao);
-    getchar();
+    limparbuffer();
 
     switch(opcao){
         case 0:
@@ -446,12 +692,7 @@ int main(){
         default:
             printf("Opcao inválida");
             break;
-    }    
-                
-    /*do{
-        menuP();
-    }while(1);
-    */
+    }
 
     return 0;
 }
